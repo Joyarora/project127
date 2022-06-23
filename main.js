bd013d643bfda855e2ae71f2a17d2a5cdf716c78
Joyarora <95618031+Joyarora@users.noreply.github.com>
2022-06-23 03:28:06 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+song1="";
+song2="";
+function preload()
+{
+    song1=loadSound("peter_pan.mp3");
+    song2=loadSound("harry_potter_theme_song.mp3")
+}
+function setup()
+{
+    canvas=createCanvas(600,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+}
+function draw()
+{
+    image(video,0,0,600,500);
+}
+function play()
+{
+    song1.play();
+    song2.play();
+}
